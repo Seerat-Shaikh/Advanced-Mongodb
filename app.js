e2a04ab7f7c4322cf86faa7889b0eaefc0f28f33
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-07-09 14:26:00 +0500

    POST requests

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const { getDb, connectToDb } = require('./db')
+const { ObjectId } = require('mongodb')
+
+// init app & middleware
+const app = express()
+app.use(express.json())
+
+// db connection
+let db
+
+//----- Connecting to Mongodb -----
+connectToDb((err) => {
+  if(!err){
+    app.listen('3000', () => {
+      console.log('app listening on port 3000')
+    })
+    db = getDb()
+  }
+})
+
+// routes
+// ----- Cursor & Fetching Data ------
+app.get('/books', (req, res) => {
+ 
+  let books = []
+
+  db.collection('books')  //cursor toArray forEach
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
+})
+
+
+//----- Finding Single Documnets -------- 
+app.get('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {
+
+    db.collection('books')
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(doc => {
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
+      
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+
+})
+
+// ---- Handling Post Requests -----
+app.post('/books', (req, res) => {
+  const book = req.body
+
+  db.collection('books')
+    .insertOne(book)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({err: 'Could not create new document'})
+    })
+})
+
+
