185b23a8980124dc537d7d1f10b686ef595ec0ef
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-07-12 00:46:10 +0500

    Connect Database

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require("mongodb");
 
 let dbConnection;
+//Mongodb Atlas Online
+let uri = 'mongodb+srv://Seerat:test123@cluster0.vk5gxdo.mongodb.net/firstDatabase?retryWrites=true&w=majority'
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
+    //MongoClient.connect("mongodb://localhost:27017/bookstore") //mongodb compass
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db();
         return cb();
@@ -16,3 +19,4 @@ module.exports = {
   },
   getDb: () => dbConnection,
 };
+
